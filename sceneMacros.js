85f01cac181d65ea2bae90a3e84838f33034eda5
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-12-01 21:42:30 +0000

    added keybind to open scene macro browser for current scene #5

--- sceneMacros.js
+++ sceneMacros.js
@@ -16,22 +16,46 @@ export default class SceneMacros {
                 // ID = ID OF SCENE
                 const element = game.release.generation >= 13 ? html : html[0]
                 const id = element.dataset.sceneId || element.dataset.entryId
-                const uiElement = {
-                    window: document.getElementsByClassName(`macrosBrowser_${id}`)
-                }
-                uiElement.currentlyOpen = uiElement.window.length ? true : false
-
-                // IF APP ALREADY OPEN ? BRING TO FRONT : RENDER NEW MACRO_BROWSER WINDOW
-                uiElement.currentlyOpen
-                    ? foundry.applications.instances.get(uiElement.window[0].id).bringToFront()
-                    : new MacroBrowserV2(id).render(true)
+                this.toggleMacrosBrowser(id)
             },
             condition: gmStatus,
             icon: '<i class="fas fa-code"></i>',
             name: "SCENE_MACROS.macro-browser.open-browser"
         }
     }
+
+    static toggleMacrosBrowser(id) {
+        const uiElement = {
+            window: document.getElementsByClassName(`macrosBrowser_${id}`)
+        }
+        uiElement.currentlyOpen = uiElement.window.length ? true : false
+
+        // IF APP ALREADY OPEN ? BRING TO FRONT : RENDER NEW MACRO_BROWSER WINDOW
+        uiElement.currentlyOpen
+            ? foundry.applications.instances.get(uiElement.window[0].id).bringToFront()
+            : new MacroBrowserV2(id).render(true)
+    }
+
 }
+
+Hooks.on('init', () => {
+
+    // OPEN / BRING TO FRONT MACRO BROWSER FOR CURRENT SCENE 
+    game.keybindings.register(SceneMacros.NAME, 'toggleMacrosBrowser', {
+        name: "Toggle Scene Macros Browser",
+        hint: "Open/close scene macros browser for the currently viewed scene.",
+        editable: [
+            {
+                key: "KeyM",
+            }
+        ],
+        onDown: () => {
+            if (!game.scenes.current) return ui.notifications.warn(`${SceneMacros.NAME}: No current scene to open sceme macros browser for.`)
+            SceneMacros.toggleMacrosBrowser(game.scenes.current.id)
+        },
+    })
+
+})
 
 // ADD MENU ITEM TO OPEN A SCENES MACRO BROWSER TO ITS CONTEXT MENU
 // V13+ HOOK
